1a8f11cbe6381bf5845fa7fbe6fb09811f5a2487
Thomas <twatson52@mac.com>
2013-07-05 16:36:58 -0500

    stop whining about parens

--- src/tpw_rules/metallicmanipulators/MachineBase.scala
+++ src/tpw_rules/metallicmanipulators/MachineBase.scala
@@ -10,16 +10,16 @@ trait BlockMachine extends BlockContainer with Front {
   override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, entity: EntityLivingBase, item: ItemStack): Unit = {
     super.onBlockPlacedBy(world, x, y, z, entity, item)
     if (world.isRemote) return
-    world.getBlockTileEntity(x, y, z).asInstanceOf[TileMachine].placed
+    world.getBlockTileEntity(x, y, z).asInstanceOf[TileMachine].placed()
   }
 
   override def breakBlock(world: World, x: Int, y: Int, z: Int, side: Int, meta: Int) = {
-    if (!world.isRemote) world.getBlockTileEntity(x, y, z).asInstanceOf[TileMachine].broken
+    if (!world.isRemote) world.getBlockTileEntity(x, y, z).asInstanceOf[TileMachine].broken()
     super.breakBlock(world, x, y, z, side, meta)
   }
 }
 
 trait TileMachine extends TileEntity {
-  def placed
-  def broken
+  def placed()
+  def broken()
 }
