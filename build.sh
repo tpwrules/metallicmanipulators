7e54ccd0e2121beee3149d49e57d2ff2a00ef182
Thomas <twatson52@mac.com>
2013-07-05 13:44:22 -0500

    add mod basics

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p build
+
+rm -r forge/mcp/src/minecraft/tpw_rules
+
+cp -r src/tpw_rules forge/mcp/src/minecraft/tpw_rules
+
+cd forge/mcp/bin
+scalac -classpath client_161.jar:../jars/libraries/com/google/guava/guava/14.0/guava-14.0.jar -sourcepath ../src -d minecraft/ ../src/minecraft/tpw_rules/crappymod/*.scala
+cd ..
+./reobfuscate.sh
+
+cd ../..
+
+mkdir -p build/zip
+rm -r build/zip/*
+mkdir build/zip/tpw_rules
+
+cp -r forge/mcp/reobf/minecraft/tpw_rules build/zip/
+
+cp -r assets build/zip/assets
+
+cp pack.mcmeta mcmod.info build/zip
+
+cd build/zip
+rm ../crappymod.jar
+zip -r ../crappymod.jar *
+cd ../..
+
+cp build/crappymod.jar ~/Library/Application\ Support/minecraft/mods/
