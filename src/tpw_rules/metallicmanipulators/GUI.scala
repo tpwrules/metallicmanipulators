214a1d2144679d1cd3d723c879c03cfd998391c2
Thomas <twatson52@mac.com>
2013-07-06 07:54:21 -0500

    generate png for metallic extractor

---
+++ src/tpw_rules/metallicmanipulators/GUI.scala
@@ -0,0 +1,12 @@
+package tpw_rules.metallicmanipulators
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomaswatson
+ * Date: 7/6/13
+ * Time: 7:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class GUI {
+
+}
