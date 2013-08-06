973583c3b5ce27a6442669368f35592c159400df
Thomas <twatson52@mac.com>
2013-08-06 09:42:53 -0500

    fix FML calling me a bad programmer

--- src/tpw_rules/metallicmanipulators/MetallicManipulators.scala
+++ src/tpw_rules/metallicmanipulators/MetallicManipulators.scala
@@ -14,16 +14,25 @@ object MetallicManipulators {
 
   val rand = new Random
 
-  val tabBlocks = new CreativeTabBlocks()
-  val tabItems = new CreativeTabItems()
+  // we need to make all of these lazy in order to initialize them
+  // at the proper time and keep them vals
 
-  val metallicExtractor = new BlockMetallicExtractor(Config.blockExtractorID)
+  lazy val tabBlocks = new CreativeTabBlocks()
+  lazy val tabItems = new CreativeTabItems()
 
-  val ironNugget = new ItemIronNugget(Config.itemIronNuggetID)
+  lazy val metallicExtractor = new BlockMetallicExtractor(Config.blockExtractorID)
 
-  NetworkRegistry.instance.registerGuiHandler(this, GUIHandler)
+  lazy val ironNugget = new ItemIronNugget(Config.itemIronNuggetID)
 
   @Mod.EventHandler
   def init(e: event.FMLInitializationEvent) = {
+    var x: Any = null
+    // ensure all the lazy vals are referenced so they are constructed
+    x = tabBlocks
+    x = tabItems
+    x = metallicExtractor
+    x = ironNugget
+
+    NetworkRegistry.instance.registerGuiHandler(this, GUIHandler)
   }
 }
